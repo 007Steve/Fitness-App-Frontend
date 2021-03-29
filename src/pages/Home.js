94b89c035a026b20a844255fc0a8af30eb18242a
007steve <stephen.plummer1.sp@gmail.com>
2021-03-29 04:54:55 -0400

    added home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  "../styles/Home.css";
+import Widget from '../components/Widget'
+import Container from '../components/Container'
+function Home() {
+    return (
+        <div className="home">
+            <Widget/>
+            <Container/>
+        </div>
+    )
+}
+
+export default Home
